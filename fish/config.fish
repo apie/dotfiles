437d712dab45e9fdd93a234a6c3213e424fce8d5
Denick <1049485+apie@users.noreply.github.com>
2019-11-27 17:16:07 +0100

    Add config for fish-shell

---
+++ fish/config.fish
@@ -0,0 +1,24 @@
+set fish_greeting
+# Git aliases
+abbr add git add
+abbr branch git branch
+abbr checkout git checkout
+abbr cherry-pick git cherry-pick
+abbr co git checkout
+abbr commit git commit
+abbr diff git diff
+abbr diffw git diffw
+abbr fetch git fetch
+abbr g git
+abbr log git log
+abbr pull git pull
+abbr push git push
+abbr pushfl git pushfl
+abbr pushup git pushup
+abbr s git status
+abbr show git show
+abbr showw git showw
+abbr stash git stash
+abbr status git status
+# Other
+abbr d ls
