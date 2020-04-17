7e129bf9cfaf4d8b0daf52b94771097b16d1e240
Denick <1049485+apie@users.noreply.github.com>
2020-04-17 11:18:44 +0200

    Add some help to "klaar"

--- fish/functions/klaar.fish
+++ fish/functions/klaar.fish
@@ -1,9 +1,18 @@
 function klaar
+    # Let the user know when a command is done. A different message for success/failure is used.
+    # Usage:
+    # 1. command | klaar
+    # 2. klaar command
+    # In the second case also the command is printed.
+
     set KLAAR 'Ik ben klaar'
     set FOUT 'Er ging iets mis'
     if eval $argv
-        espeak -v nl "$KLAAR"; and notify-send "$KLAAR"
+        espeak -v nl "$KLAAR"
+        and notify-send "$KLAAR" "$argv"
+
     else
-        espeak -v nl "$FOUT"; and notify-send "$FOUT"
+        espeak -v nl "$FOUT"
+        and notify-send "$FOUT" "$argv"
     end
 end
