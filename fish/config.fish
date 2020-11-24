2a00f461a09f3c7b2306e07c9fe326c9b5a6aa9b
Denick <1049485+apie@users.noreply.github.com>
2020-11-24 15:26:58 +0100

    Abbreviation for running command in pre-deploy

--- fish/config.fish
+++ fish/config.fish
@@ -34,6 +34,7 @@ abbr avc ssh apie@denickm-vps
 abbr cropr "cd ~/clones/cropr_project/; and source ~/.virtualenvs/cropr_project/bin/activate.fish"
 abbr cloudfarm "cd ~/clones/cloud_farm/; and source ~/.virtualenvs/cloud_farm/bin/activate.fish"
 abbr sp ./manage.py shell_plus --quiet-load --ptipython
+abbr com docker-compose run --rm pre-deploy
 
 # Fish autocomplete for manage.py
 __fish_complete_django manage.py
