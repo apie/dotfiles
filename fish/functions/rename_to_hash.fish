8d3f6278ed42b7a931abfae86b32264a6f8cbcba
Denick <1049485+apie@users.noreply.github.com>
2024-04-03 19:51:42 +0200

    improve rename_to_hash

--- fish/functions/rename_to_hash.fish
+++ fish/functions/rename_to_hash.fish
@@ -1,5 +1,11 @@
+# Use something like this to find and rename files that are not yet renamed:
+# rename_to_hash (find . -maxdepth 1 -type f -name '*' -regextype posix-extended ! -regex '^\./.{32}\..{3,4}')
+
 function rename_to_hash
-    ls $argv
+    # Argument required
+    test -z "$argv"; and return 1;
+    # Argument must be existing file/dir
+    ls $argv; or return 1;
     # pause here and print list. break cancels the operation.
     read --prompt-str 'Ik ga bovenstaande files hernoemen. OK?'; or return 1
     md5sum $argv | 
