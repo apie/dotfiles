e9292b916ed759c6c859ed0ae3e1336a15473d78
Denick <1049485+apie@users.noreply.github.com>
2024-10-23 15:16:40 +0200

    update activate function
    Try 3 different paths
    Use exit codes

--- fish/functions/activate.fish
+++ fish/functions/activate.fish
@@ -1,4 +1,6 @@
 function activate
-    source ~/.virtualenvs/(basename (pwd))/bin/activate.fish
-    or source venv/bin/activate.fish
+    source ~/.virtualenvs/(basename (pwd))/bin/activate.fish 2>/dev/null; and return 0
+    or source venv/bin/activate.fish 2>/dev/null; and return 0
+    or source virtual/bin/activate.fish 2>/dev/null; and return 0
+    or echo 'Nothing found to activate' >&2; and return 1
 end
