37c508f5ec036c1d758a7e79acd943f8975d6da8
Denick <1049485+apie@users.noreply.github.com>
2025-07-29 14:41:14 +0200

    handle names with spaces better

--- fish/functions/rename_to_hash.fish
+++ fish/functions/rename_to_hash.fish
@@ -8,8 +8,8 @@ function rename_to_hash
     ls $argv; or return 1;
     # pause here and print list. break cancels the operation.
     read --prompt-str 'Ik ga bovenstaande files hernoemen. OK?'; or return 1
-    md5sum $argv | 
-       while read -d"  " sum filename do
-           mv -v "$filename" (path dirname "$filename")/"$sum"(path extension "$filename")
-       end
+    for filename in $argv
+       md5sum $filename | read --nchars 32 sum
+       mv -v "$filename" (path dirname "$filename")/"$sum"(path extension "$filename")
+    end
 end
